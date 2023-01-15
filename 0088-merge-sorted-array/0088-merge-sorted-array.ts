d286ed0aa38cb62411e2dbcd24256b57b4143dfa
Yu Takaki <51910423+YuTakaki@users.noreply.github.com>
2023-01-15 12:26:08 +0900

    Time: 78 ms (53.93%), Space: 43.6 MB (72.10%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -3,18 +3,28 @@
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
   // create a sorted array to store merge arrays
-  while(m && n){
-        let i = m + n - 1;
-        if (nums1[m-1] > nums2[n-1]) {
-            nums1[i] = nums1[m-1]
-            m--;
-        } else {
-            nums1[i] = nums2[n-1]
-            n--
-        }
+  const nums1Copy: any[] = [...nums1];
+  let index = 0;
+  while (m > 0 && n > 0 && nums1Copy.length > 0 && nums2.length > 0) {
+    if (nums1Copy[0] < nums2[0]) {
+      nums1[index] = nums1Copy.shift();
+      m--;
+    } else {
+      nums1[index] = nums2.shift()!;
+      n--;
     }
-    while(n){
-        nums1[m + n - 1] = nums2[n-1];
-        n--;
-    }
+    index++;
+  }
+
+  while (m > 0) {
+    m--;
+    nums1[index] = nums1Copy.shift();
+    index++;
+  }
+
+  while (n > 0) {
+    n--;
+    nums1[index] = nums2.shift()!;
+    index++;
+  }
 }
