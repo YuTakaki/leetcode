45143e35bfa79eeab7e2671b49497adcef2d26f0
yu-dev <51910423+yuudev14@users.noreply.github.com>
2023-04-14 23:20:54 +0900

    Time: 108 ms (48.94%), Space: 49.2 MB (47.16%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -3,7 +3,6 @@ function lengthOfLongestSubstring(s: string): number {
     let left = 0;
     let right = 0;
     const cache = {}
-    let i = 0
     while (right < s.length) {
         if(s[right] in cache) {
             left = Math.max(cache[s[right]], left)
