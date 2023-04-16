4eff3256e80e77d1d4380c3b0dfd858fddd8bb30
yu-dev <51910423+yuudev14@users.noreply.github.com>
2023-04-16 22:19:05 +0900

    Time: 57 ms (94.27%), Space: 44.6 MB (64.49%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.ts
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.ts
@@ -10,23 +10,43 @@
  * }
  */
 
-function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
-    let curr = head;
-    const cache = []
+// function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+//     let curr = head;
+//     const cache = []
     
-    while(curr != null) {
-        cache.push(curr)
-        curr = curr.next;
+//     while(curr != null) {
+//         cache.push(curr)
+//         curr = curr.next;
+//     }
+//     const index = (cache.length - n) - 1;
+//     if (cache.length === 1) {
+//         head = null
+//     } else if (n === 1) {
+//         cache[index].next = null
+//     } else if (cache.length === n) {
+//         head = head.next;
+//     } else {
+//         cache[index].next = cache[index].next.next
+//     }
+//     return head;
+// };
+
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+    let right = head;
+    let left = head;
+    
+    for (let i = 0; i < n; i++) {
+        right = right.next;
     }
-    const index = (cache.length - n) - 1;
-    if (cache.length === 1) {
-        head = null
-    } else if (n === 1) {
-        cache[index].next = null
-    } else if (cache.length === n) {
-        head = head.next;
-    } else {
-        cache[index].next = cache[index].next.next
+    if (!right) return head.next;
+    
+    while (right.next) {
+        right = right.next;
+        left = left.next;
     }
-    return head;
+    
+    left.next = left.next.next
+    return head
+    
 };
