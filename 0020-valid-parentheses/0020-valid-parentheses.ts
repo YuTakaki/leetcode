ed40051c9f0eb003c8852beb2618f8d2b93374a1
yu-dev <51910423+yuudev14@users.noreply.github.com>
2023-05-13 23:34:05 +0900

    Time: 70 ms (48.45%), Space: 45.7 MB (10.80%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.ts
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -7,15 +7,14 @@ function isValid(s: string): boolean {
     }
     
     for (let c of s) {
-        if(!(c in charsMap)) {
-            chars.push(c)
-        } else {
+        if(c in charsMap) {
             const openingChar = chars.pop()
             if (charsMap[c] !== openingChar) {
-                console.log(chars, c, openingChar)
                 return false
             }
-        } 
+        } else {
+            chars.push(c)   
+        }
     }
     
     return chars.length === 0
