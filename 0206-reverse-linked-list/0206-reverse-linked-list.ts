d563a29307bbfe9cc863768b823ddad2130bf027
yu-dev <51910423+yuudev14@users.noreply.github.com>
2023-06-29 21:15:08 +0900

    Time: 69 ms (53.33%), Space: 44.1 MB (99.63%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.ts
+++ 0206-reverse-linked-list/0206-reverse-linked-list.ts
@@ -11,29 +11,45 @@
  */
 
 function reverseList(head: ListNode | null): ListNode | null {
-    // push val in the array
-    const array = []
     let curr = head
-    while (curr) {
-        array.push(curr.val)
-        curr = curr.next
+    let next = null
+    let prev = null
+    
+    while(curr) {
+        next = curr.next
+        curr.next = prev
+        
+        prev = curr
+        curr = next  
     }
-    
-    // create a new Object
-    let newNode = null
-    let tail = newNode
-    
-    for (let i = array.length - 1; i >= 0; i--){
-        const node = new ListNode(array[i], null)
-        if (tail !== null) {
-            tail.next = node
-            tail = tail.next
-        }else {
-            newNode = node
-            tail = newNode
-        }
-    }
-    
-    return newNode
+    return prev
 
 };
+
+// function reverseList(head: ListNode | null): ListNode | null {
+//     // push val in the array
+//     const array = []
+//     let curr = head
+//     while (curr) {
+//         array.push(curr.val)
+//         curr = curr.next
+//     }
+    
+//     // create a new Object
+//     let newNode = null
+//     let tail = newNode
+    
+//     for (let i = array.length - 1; i >= 0; i--){
+//         const node = new ListNode(array[i], null)
+//         if (tail !== null) {
+//             tail.next = node
+//             tail = tail.next
+//         }else {
+//             newNode = node
+//             tail = newNode
+//         }
+//     }
+    
+//     return newNode
+
+// };
