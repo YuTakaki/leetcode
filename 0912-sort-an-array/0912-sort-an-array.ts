4f40ef4638630984db644f7b2f09e9a2a4124e37
yu-dev <51910423+yuudev14@users.noreply.github.com>
2023-08-03 10:03:04 +0900

    Time: 3983 ms (18.95%), Space: 63.9 MB (85.89%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.ts
@@ -0,0 +1,14 @@
+function sortArray(nums: number[]): number[] {
+    for (let x = 1; x < nums.length; x++) {
+        for (let y = x; y > 0; y--) {
+            if (nums[y] < nums[y-1]){
+                const temp = nums[y]
+                nums[y] = nums[y-1]
+                nums[y-1] = temp
+            }else {
+                break
+            }
+        }
+    }
+    return nums
+};
