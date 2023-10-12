38ccf911a2064e4b113d786b299a896a5ec30446
yu-dev <51910423+yuudev14@users.noreply.github.com>
2023-10-12 22:53:51 +0900

    Time: 58 ms (40.80%), Space: 42.2 MB (87.25%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.ts
@@ -0,0 +1,24 @@
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function(isBadVersion: any) {
+
+    return function(n: number): number {
+        let l = 0;
+        let r = n
+        while (l <= r) {
+            const pivot = Math.floor((l + r) / 2)
+            const isbad = isBadVersion(pivot)
+            if (isbad) {
+                r = pivot - 1
+            } else {
+                l = pivot + 1
+            }
+        }
+        return Math.floor((l + r) / 2) + 1
+    };
+};
